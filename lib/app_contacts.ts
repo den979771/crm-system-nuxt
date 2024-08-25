9f320722f70464ad422f033e19ff898cfb20b602
den979771 <den979771@gmail.com>
2024-08-25 11:35:09 +0300

    finish setting appwrite

---
+++ lib/app_contacts.ts
@@ -0,0 +1,6 @@
+export const APP_WRITE_ID = "66c8c46700218f04cb1a";
+export const DB_ID = "crm-base";
+export const COLLECTION_DEALS = "deals";
+export const COLLECTION_CUSTOMERS = "customers";
+export const COLLECTION_COMMENTS = "comments";
+export const STORAGE_ID = "storage";
